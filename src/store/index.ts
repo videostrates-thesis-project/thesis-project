2167b17c1bfb9bbc610a1f3afb7d444c4bb5649e
Anna Győrffy <52412232+annesparks@users.noreply.github.com>
2024-02-01 11:43:45 +0100

    Initial commit

---
+++ src/store/index.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export interface AppState {
+  counter: number;
+  setCounter: (counter: number) => void;
+}
+
+export const useStore = create(
+  persist<AppState>(
+    (set) => ({
+      counter: 0,
+      setCounter: (counter) => set({ counter }),
+    }),
+    {
+      name: "thesis-project-storage",
+    }
+  )
+);
