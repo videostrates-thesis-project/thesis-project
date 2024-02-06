6c18d47c2759e2a78c6577a96bbb60e798ae4f18
Jędrzej Głowaczewski <jedrzej.glowaczewski@gmail.com>
2024-02-06 12:13:29 +0100

    Embed a video player as a webstrate

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
 import "./App.css"
-import React from "react"
 
 function App() {
-  const textareaRef = React.useRef<HTMLTextAreaElement>(null)
-  const iframeRef = React.useRef<HTMLIFrameElement>(null)
-
-  function updateVideostrate() {
-    const iframeWindow = iframeRef.current?.contentWindow
-    iframeWindow?.postMessage(
-      {
-        type: "videostrate",
-        payload: {
-          text: textareaRef.current?.value,
-        },
-      },
-      "*"
-    )
-  }
-
   return (
     <div className="flex flex-col gap-4 w-full">
       <iframe
-        ref={iframeRef}
-        // src="https://demo.webstrates.net/tidy-catfish-28/"
-        src="https://demo.webstrates.net/chatty-tiger-57/"
-        title="W3Schools Free Online Web Tutorials"
+        className="w-full h-[50rem]"
+        src="https://demo.webstrates.net/dry-eel-83/"
       ></iframe>
-      <textarea id="text" ref={textareaRef} />
-      <button className="btn btn-primary" onClick={updateVideostrate}>
-        Update webstrate content
-      </button>
     </div>
   )
 }
