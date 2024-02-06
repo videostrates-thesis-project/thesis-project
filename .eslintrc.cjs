2946ad0034f4d590c1186e5a634afa5f3845972d
Jędrzej Głowaczewski <jedrzej.glowaczewski@gmail.com>
2024-02-06 12:00:26 +0100

    Remove warnings about wrong line endings on windows

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,20 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, node:true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
-    'plugin:prettier/recommended',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "plugin:prettier/recommended",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
   },
 }
