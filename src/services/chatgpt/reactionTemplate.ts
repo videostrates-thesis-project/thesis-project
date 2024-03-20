59075c4fe80fa903950e2559a1c0f2b13dc22302
Marcell Tóth <tmarcellt.2000@gmail.com>
2024-03-20 12:24:55 +0100

    Added image name generation

--- src/services/chatgpt/reactionTemplate.ts
+++ src/services/chatgpt/reactionTemplate.ts
@@ -1,3 +1,3 @@
 export const buildReactionMessage = (input: string) => {
-  return `You should decide whether to create a reaction to the user's latest prompt: ${input}. The reaction can only be a single emoji character. E.g. 🙈, 🐶, 😄, 😞, etc. Be creative with the emojis. For example, if the user makes repetitive requests, you can react with a tired emoji 😴. If it doesn't make sense to leave a reaction on this message, respond with "No reaction", otherwise react with only one emoji character and nothing else.`
+  return `You should decide whether to create a reaction to the next user prompt, which is: ${input}. The reaction can only be a single emoji character. E.g. 🙈, 🐶, 😄, 😞, etc. Be creative with the emojis. For example, if the user makes repetitive requests, you can react with a tired emoji 😴. If it doesn't make sense to leave a reaction on this message, respond with "No reaction", otherwise react with only one emoji character and nothing else.`
 }
