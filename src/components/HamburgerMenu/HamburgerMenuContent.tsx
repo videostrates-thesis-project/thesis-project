e7c7bad619e5a4bd1d8e3e9ceba6bd8428a7a711
Anna Győrffy <52412232+annesparks@users.noreply.github.com>
2024-04-20 10:32:45 +0200

    feat: Import and export store

--- src/components/HamburgerMenu/HamburgerMenuContent.tsx
+++ src/components/HamburgerMenu/HamburgerMenuContent.tsx
@@ -1,14 +1,47 @@
-import { useCallback, useState } from "react"
+import { useCallback, useRef, useState } from "react"
 import { useStore } from "../../store"
 
 const HamburgerMenuContent = () => {
   const { videostrateUrl, setVideostrateUrl, setShowScriptTab, showScriptTab } =
     useStore()
   const [url, setUrl] = useState(videostrateUrl)
+  const downloadRef = useRef<HTMLAnchorElement>(null)
+  const importRef = useRef<HTMLInputElement>(null)
 
   const onChangeUrl = useCallback(() => {
     setVideostrateUrl(url)
   }, [setVideostrateUrl, url])
+
+  const onExport = useCallback(() => {
+    const store = useStore.getState()
+    const json = JSON.stringify(store)
+
+    if (downloadRef.current) {
+      downloadRef.current.href = `data:text/json;charset=utf-8,${encodeURIComponent(json)}`
+
+      downloadRef.current.click()
+    }
+  }, [])
+
+  const onImport = useCallback(() => {
+    if (importRef.current) {
+      importRef.current.click()
+    }
+  }, [])
+
+  const onUpload = useCallback(
+    async (e: React.ChangeEvent<HTMLInputElement>) => {
+      const files = e.target.files
+      if (!files || files.length <= 0) {
+        return
+      }
+      const file = files[0]
+      const store = JSON.parse(await file.text())
+      useStore.setState(store)
+      window.location.reload()
+    },
+    []
+  )
 
   return (
     <>
@@ -36,6 +69,29 @@ const HamburgerMenuContent = () => {
           <span className="label-text ml-2">Show script tab</span>
         </label>
       </div>
+
+      <div className="flex flex-row gap-4">
+        <button className="btn btn-accent w-auto" onClick={onExport}>
+          Export store
+        </button>
+        <button className="btn btn-info w-auto" onClick={onImport}>
+          Import store
+        </button>
+      </div>
+
+      <a
+        className="hidden"
+        ref={downloadRef}
+        href={""}
+        download="store.json"
+      ></a>
+
+      <input
+        ref={importRef}
+        type="file"
+        className="hidden"
+        onChange={onUpload}
+      />
     </>
   )
 }
